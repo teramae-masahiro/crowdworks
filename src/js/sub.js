bc12f0e49d1394eec02acbd26a3998ee5febdcf6
masahiro <mmaassaa0618@gmail.com>
2022-11-20 12:39:12 +0900

    add observer

--- src/js/sub.js
+++ src/js/sub.js
@@ -1,9 +1,25 @@
 import { Slider } from './class/swiper';
+import { ScrollObserver } from './class/observer';
 import '@scss/reset/reset';
 
+
+//slider
 const slider = new Slider(".swiper");
 slider.start();
-// slider.swiper.on('slideChange', function () {
-//     console.log('slide changed');
-//     console.log(slider.swiper);
-// });
+slider.swiper.on('slideChange', function () {
+    console.log('slide changed');
+    console.log(slider.swiper);
+});
+
+//observer
+const observerCallback =(el, isIntersecting) => {
+  if (isIntersecting) {
+    console.log('hello');
+  } else {
+    console.log('bye');
+  }
+};
+new ScrollObserver(".target", observerCallback, {
+  once: false,
+  threshold: 0.5,
+});
